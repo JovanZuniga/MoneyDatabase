480f3cddd51ce74cf976a3248f6eb669762e88a4
Jovan Zuniga <jovartzy@proton.me>
2025-11-06 01:21:36 -0500

    Splits amount into dollars and cents

--- MoneyDatabase.py
+++ MoneyDatabase.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 root = Tk()
 root.title("Money Spent Database")
 root.geometry("500x500")
-root.config(bg="#38f7f7")
+root.config(bg="#000000")
 root.wm_attributes("-topmost", True)
 
 #Create Database
@@ -14,7 +14,8 @@ c = conn.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS transactions (
         id integer PRIMARY KEY AUTOINCREMENT,
         card_name text NOT NULL,
-        amount integer NOT NULL,
+        amountDollars integer NOT NULL,
+        amountCents integer NOT NULL, 
         description text NOT NULL
           )
           """)
@@ -24,18 +25,20 @@ conn.close()
 def submit():
     conn = sqlite3.connect('transactions.db')
     c = conn.cursor()
-    c.execute(" INSERT INTO transactions VALUES (:id, :card_name, :amount, :description)", 
+    c.execute(" INSERT INTO transactions VALUES (:id, :card_name, :amountDollars, :amountCents, :description)", 
               {
                 'id': None,
                   'card_name': card_cbox.get(),
-                  'amount': amount.get(),
+                  'amountDollars': amtDollars.get(),
+                  'amountCents': amtCents.get(),
                   'description': descrip.get()
               })
     conn.commit()
     conn.close()
 
     card_cbox.set('Select a Card')
-    amount.delete(0, END)
+    amtDollars.delete(0, END)
+    amtCents.delete(0, END)
     descrip.delete(0, END)
 
 #Query the database
@@ -65,34 +68,52 @@ vcmd = (root.register(validate), '%P')
 text_var1 = StringVar()
 text_var2 = StringVar()
 text_var3 = StringVar()
+text_var4 = StringVar()
 
 
 #Create Layout
 topFrame = Frame(root)
-topFrame.pack(side='top', padx=10, pady=(10, 5))
+topFrame.pack(side='top', padx=20, pady=(20, 5), fill='both')
+topFrame.columnconfigure(0, weight=1)
+# topFrame.columnconfigure(1, weight=1)
+topFrame.columnconfigure(2, weight=1)
 
-bottomFrame = Frame(root, )
+bottomFrame = Frame(root)
 bottomFrame.pack(side='bottom', padx=10, pady=(5, 10))
 
+titleLbl = Label(topFrame, text="Enter a New Record")
+titleLbl.grid(column=0, row=0, columnspan=3)
 #Dropdown List
 card_options = ["Discover Debit", "Discover Credit", "Capital One", "Blue Cash Everyday", "Wells Fargo", "Paypal", "Costco"]
 
 card_cbox = ttk.Combobox(topFrame, values=card_options, textvariable=text_var3)
 card_cbox.set("Select a Card")
 card_cbox['state'] = 'readonly'
-card_cbox.pack()
+card_cbox.grid(row=3, column=1)
+
+amtFrame = Frame(topFrame)
+amtFrame.grid(row=1, column=1, columnspan=3)
+
+
+amtLbl = Label(amtFrame, text="Amount:   $")
+amtLbl.pack(side='left')
 
 #Entry
-amount = Entry(topFrame, width=5, textvariable=text_var1, validate="key", validatecommand=vcmd)
-amount.pack()
+amtDollars = Entry(amtFrame, width=5, textvariable=text_var1, validate="key", validatecommand=vcmd)
+amtDollars.pack(side='left')
+amtLbl2 = Label(amtFrame, text='.')
+amtLbl2.pack(side='left')
+amtCents = Entry(amtFrame, width=2, textvariable=text_var4)
+amtCents.pack(side='left')
 
 
 descrip = Entry(topFrame, width=20, textvariable=text_var2)
-descrip.pack()
+descrip.grid(row=2, column=1)
 
 
 def clear():
-    amount.delete(0, END)
+    amtDollars.delete(0, END)
+    amtCents.delete(0, END)
     descrip.delete(0, END)
     card_cbox.set("Select a Card")
 
@@ -100,21 +121,22 @@ def clear():
 #Submit Button
 submitBtn = Button(topFrame, text="Submit Record", command=submit)
 submitBtn.config(state='disabled')
-submitBtn.pack()
+submitBtn.grid(row=4, column=1)
 
 def checkButton(*args):
-    if text_var1.get() and text_var2.get() != '' and card_cbox.get() != "Select a Card":
+    if text_var1.get() and text_var2.get() and text_var4.get() != '' and card_cbox.get() != "Select a Card":
         submitBtn.config(state='normal')
     else:
         submitBtn.config(state='disabled')
 
 text_var1.trace('w', checkButton)
 text_var2.trace('w', checkButton)
+text_var4.trace('w', checkButton)
 card_cbox.bind('<<ComboboxSelected>>', checkButton)
 
 #Clear Button
 clearBtn = Button(topFrame, text="Clear", command=clear)
-clearBtn.pack()
+clearBtn.grid(row=5, column=1)
 
 #Query Button
 queryBtn = Button(bottomFrame, text="Load Records", command=query)
